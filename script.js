294351973b27bb375b1efa234e0f164790782c10
DiegoGomez <diegocor4@gmail.com>
2023-05-31 00:27:06 -0300

    boton encriptar en proceso

---
+++ script.js
@@ -0,0 +1,6 @@
+function encriptar() {
+    //alert('ningun mensaje fue encontrado');
+    var elemento = document.querySelector('.mensaje-muneco p:first-child');
+    elemento.style.display = 'block';
+
+}
